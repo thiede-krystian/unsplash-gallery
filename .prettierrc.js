2b9f1301d76e21ea928fccdd06303a365ca5878e
thiede-krystian <krystian.thiede@protonmail.com>
2025-01-22 13:24:25 +0100

    chore(dev-deps): add Prettier and ESLint configuration with updates to .gitignore and package files
    - format files

---
+++ .prettierrc.js
@@ -0,0 +1,16 @@
+/** @type {import("prettier").Config} */
+const config = {
+  printWidth: 120,
+  singleQuote: true,
+  trailingComma: 'none',
+  semi: true,
+  arrowParens: 'always',
+  bracketSpacing: true,
+  tabWidth: 2,
+  useTabs: false,
+  jsxSingleQuote: true,
+  jsxBracketSameLine: false,
+  endOfLine: 'lf'
+};
+
+module.exports = config;
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,22 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
+import prettierPlugin from 'eslint-plugin-prettier'; // import required
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+  baseDirectory: __dirname
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends('next/core-web-vitals', 'next/typescript', 'plugin:prettier/recommended'),
+  {
+    plugins: {
+      prettier: prettierPlugin
+    }
+  }
 ];
 
 export default eslintConfig;
